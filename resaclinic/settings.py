32eb18b14df0e5ea11a0bae9967e2edd41c8fdf0
mrisho2348 <mrishohamisi2348@gmail.com>
2024-04-19 12:25:59 +0300

    on new computer at university

--- resaclinic/settings.py
+++ resaclinic/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER':'root',
         'PASSWORD':'m2r3i4s8',            
         'HOST':'localhost',
-        'PORT':'3306',
+        'PORT':'3307',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
